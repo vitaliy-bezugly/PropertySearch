71e8bc8d7efcbcb94d384e8f9062a0448fea4c6c
Vitaliy <bezuglivita123@gmail.com>
2023-02-28 11:55:06 +0200

    Added identity

--- PropertySearchApp/Program.cs
+++ PropertySearchApp/Program.cs
@@ -1,5 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using PropertySearchApp.Areas.Identity.Data;
+using PropertySearchApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("LocalDatabaseConnection") 
+    ?? throw new InvalidOperationException("Connection string 'LocalDatabaseConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<UserEntity>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -17,11 +31,14 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
