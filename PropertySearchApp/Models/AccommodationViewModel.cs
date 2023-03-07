05c1c468060bae6927b4988a10b2d01717ef2f41
Vitaliy <bezuglivita123@gmail.com>
2023-03-07 18:50:09 +0200

    Accommodation view model

---
+++ PropertySearchApp/Models/AccommodationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace PropertySearchApp.Models;
+
+public class AccommodationViewModel
+{
+    public Image? MainPhoto { get; set; }
+    [Required]
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public int Price { get; set; }
+    [Required, EmailAddress]
+    public string LandlordEmail { get; set; }
+}
