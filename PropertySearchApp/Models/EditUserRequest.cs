85b66e54ecf031f20d8cd0b694cf5d4b435fc860
Vitaliy <bezuglivita123@gmail.com>
2023-04-07 16:26:36 +0300

    separated edit and details page

--- PropertySearchApp/Models/EditUserRequest.cs
+++ PropertySearchApp/Models/EditUserRequest.cs
@@ -4,5 +4,12 @@ public class EditUserRequest
 {
     public Guid Id { get; set; }
     public string UserName { get; set; }
+    public string Information { get; set; }
+    public List<ContactViewModel> Contacts { get; set; }
     public string Email { get; set; }
+
+    public EditUserRequest()
+    {
+        Contacts = new List<ContactViewModel>();
+    }
 }
