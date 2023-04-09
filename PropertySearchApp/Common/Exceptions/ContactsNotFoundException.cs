ff898163597f55388f25c1ba236783e59b336f4d
Vitaliy <bezuglivita123@gmail.com>
2023-04-09 15:49:40 +0300

    contact repository

---
+++ PropertySearchApp/Common/Exceptions/ContactsNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using PropertySearchApp.Common.Exceptions.Abstract;
+
+namespace PropertySearchApp.Common.Exceptions;
+
+public class ContactsNotFoundException : BaseApplicationException
+{
+    public ContactsNotFoundException(string message) : base(message)
+    {
+        
+    }
+}
