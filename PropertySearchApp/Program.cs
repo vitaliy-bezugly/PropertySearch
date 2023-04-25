46096e2da17f0d3be2289493096a3f0313159fa7
Vitalii Minaev <bezuglivita123@gmail.com>
2023-04-25 14:49:45 +0300

    Update Program.cs

--- PropertySearchApp/Program.cs
+++ PropertySearchApp/Program.cs
@@ -7,5 +7,6 @@ ILogger<Startup> logger = builder.CreateLogger<Startup>();
 var startup = new Startup(builder.Configuration, logger);
 startup.ConfigureServices(builder.Services);
 
+// build application
 var app = builder.Build();
 startup.Configure(app, app.Environment);
